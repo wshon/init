ad58444db4f83ae2d1313ca773f7fc86906556af
muumlover <muumlover@live.com>
2019-06-28 10:24:41 +0800

    Update debian_sources.sh

--- debian_sources.sh
+++ debian_sources.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # http://mirrors.ustc.edu.cn/help/debian.html
 cp /etc/apt/sources.list /etc/apt/sources.list.bak
 sudo sed -i 's/deb.debian.org/mirrors.ustc.edu.cn/g' /etc/apt/sources.list
