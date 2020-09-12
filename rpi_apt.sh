fa80576206fcbd47506559b040a7f04f99fa82cb
muumlover <muumlover@live.com>
2020-09-12 16:20:45 +0800

    Create rpi_apt.sh

---
+++ rpi_apt.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+mv /etc/apt/sources.list /etc/apt/sources.list.$(date +%s)
+cat >> /etc/apt/sources.list <<EOF
+deb http://mirrors.tuna.tsinghua.edu.cn/raspbian/raspbian/ buster main non-free contrib rpi
+deb-src http://mirrors.tuna.tsinghua.edu.cn/raspbian/raspbian/ buster main non-free contrib rpi
+EOF
+
+mv /etc/apt/sources.list.d/raspi.list /etc/apt/sources.list.d/raspi.list.$(date +%s)
+cat >> /etc/apt/sources.list.d/raspi.list <<EOF
+deb http://mirrors.tuna.tsinghua.edu.cn/raspberrypi/ buster main ui
+EOF
