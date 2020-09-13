83c52124a4d13011ce660930bae05731fc0d1262
muumlover <muumlover@live.com>
2020-09-13 08:05:40 +0800

    Update sshd_port.sh

--- sshd_port.sh
+++ sshd_port.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-sed -i "/Port /cPort 33322" /etc/ssh/sshd_config
+sed -i "/Port /cPort 2233" /etc/ssh/sshd_config
 systemctl restart sshd
+
+if grep -Eqii "CentOS" /etc/issue || grep -Eq "CentOS" /etc/*-release; then
+    firewall-cmd --zone=public --add-port=2233/tcp --permanent
+    firewall-cmd --reload
+    semanage port -a -t ssh_port_t -p tcp 2233
+fi
