7a8d055600257bb29795f62af1c547510f693488
wshon <muumlover@live.com>
2020-11-27 20:33:26 +0800

    Update rpios_apt.sh

--- rpios_apt.sh
+++ rpios_apt.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
-mv /etc/apt/sources.list /etc/apt/sources.list.$(date +%s)
-cat >> /etc/apt/sources.list <<EOF
-deb http://mirrors.tuna.tsinghua.edu.cn/raspbian/raspbian/ buster main non-free contrib rpi
-deb-src http://mirrors.tuna.tsinghua.edu.cn/raspbian/raspbian/ buster main non-free contrib rpi
-EOF
+cp /etc/apt/sources.list /etc/apt/sources.list.bak$(date +%s)
+cp /etc/apt/sources.list.d/raspi.list /etc/apt/sources.list.d/raspi.list.bak$(date +%s)
 
-mv /etc/apt/sources.list.d/raspi.list /etc/apt/sources.list.d/raspi.list.$(date +%s)
-cat >> /etc/apt/sources.list.d/raspi.list <<EOF
-deb http://mirrors.tuna.tsinghua.edu.cn/raspberrypi/ buster main ui
-EOF
+sed -i "s|http://raspbian.raspberrypi.org/raspbian/|https://mirrors.sjtug.sjtu.edu.cn/raspbian/raspbian/|g" /etc/apt/sources.list
+sed -i "s|http://deb.debian.org/debian|https://mirrors.sjtug.sjtu.edu.cn/debian|g" /etc/apt/sources.list
+sed -i "s|http://archive.raspberrypi.org/debian/|https://mirrors.sjtug.sjtu.edu.cn/raspberrypi/debian/|g" /etc/apt/sources.list.d/raspi.list
 
 apt update
