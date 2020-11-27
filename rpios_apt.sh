fd4af6f239dd2e8ff6ced67e7bf699188986e719
wshon <muumlover@live.com>
2020-11-27 20:35:43 +0800

    Update rpios_apt.sh

--- rpios_apt.sh
+++ rpios_apt.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-cp /etc/apt/sources.list /etc/apt/sources.list.bak$(date +%s)
-cp /etc/apt/sources.list.d/raspi.list /etc/apt/sources.list.d/raspi.list.bak$(date +%s)
+cp /etc/apt/sources.list /etc/apt/sources.list.$(date +%s).bak
+cp /etc/apt/sources.list.d/raspi.list /etc/apt/sources.list.d/raspi.list.$(date +%s).bak
 
 sed -i "s|http://raspbian.raspberrypi.org/raspbian/|https://mirrors.sjtug.sjtu.edu.cn/raspbian/raspbian/|g" /etc/apt/sources.list
 sed -i "s|http://deb.debian.org/debian|https://mirrors.sjtug.sjtu.edu.cn/debian|g" /etc/apt/sources.list
